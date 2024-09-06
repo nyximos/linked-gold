e3aaa728b2c3e4e9a9aab6683448a1ddc95abaec
nyximos <nyximos@gmail.com>
2024-09-06 15:30:37 +0900

    chore: resource-service 프로젝트 환경 구성

---
+++ resource-service/src/main/java/com/gold/ResourceServiceApplication.java
@@ -0,0 +1,19 @@
+package com.gold;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class ResourceServiceApplication {
+
+	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
+		SpringApplication.run(ResourceServiceApplication.class, args);
+	}
+
+}
