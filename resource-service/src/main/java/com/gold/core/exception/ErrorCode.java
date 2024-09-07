f1f202ec26d66ad0b0faa590978186b9a229efc4
nyximos <nyximos@gmail.com>
2024-09-08 00:13:15 +0900

    feat: InvalidPasswordException 추가 (#6)

--- resource-service/src/main/java/com/gold/core/exception/ErrorCode.java
+++ resource-service/src/main/java/com/gold/core/exception/ErrorCode.java
@@ -9,7 +9,8 @@ public enum ErrorCode {
     NOT_FOUND("해당 자원을 찾을 수 없습니다."),
     BAD_REQUEST("잘못된 요청입니다."),
     EMAIL_ALREADY_IN_USE("이미 사용중인 이메일입니다."),
-    USER_NOT_FOUND_EXCEPTION("유저를 찾을 수 없습니다.")
+    USER_NOT_FOUND_EXCEPTION("유저를 찾을 수 없습니다."),
+    INVALID_PASSWORD("잘못된 비밀번호입니다.")
     ;
 
     private String defaultMessage;
