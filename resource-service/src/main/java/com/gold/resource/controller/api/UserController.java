887c14b1b7692e90b5f63d7984c5f0dcc6c98d9a
nyximos <nyximos@gmail.com>
2024-09-08 16:32:48 +0900

    feat: 토큰 검증 (#8)

--- resource-service/src/main/java/com/gold/resource/controller/api/UserController.java
+++ resource-service/src/main/java/com/gold/resource/controller/api/UserController.java
@@ -1,23 +1,27 @@
 package com.gold.resource.controller.api;
 
+import com.gold.client.AuthClient;
 import com.gold.core.wrapper.ResultResponse;
 import com.gold.resource.controller.model.request.LoginRequestModel;
 import com.gold.resource.controller.model.request.SignUpRequestModel;
 import com.gold.resource.controller.model.response.TokenModel;
 import com.gold.resource.service.UserService;
+import com.nyximos.auth.Auth;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
+import static com.gold.core.constant.ResourceConstants.HEADER_AUTHORIZE_TOKEN;
+
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/apis/users")
 public class UserController {
 
     private final UserService userService;
+    private final AuthClient authClient; // Todo 낸주 삭제
 
     @PostMapping
     public ResultResponse<Void> signUp(@Valid @RequestBody SignUpRequestModel signUpRequestModel) {
@@ -29,5 +33,15 @@ public class UserController {
     public ResultResponse<TokenModel> signIn(@Valid @RequestBody LoginRequestModel loginRequestModel) {
         return new ResultResponse(userService.signIn(loginRequestModel));
     }
+
+    @GetMapping("/test")
+    public ResultResponse<Void> test(@RequestHeader(value = HEADER_AUTHORIZE_TOKEN) String accessToken) {
+        Auth.ValidateTokenResponse response = authClient.validateToken(accessToken);
+        if (response.getIsValid()) {
+            log.info(response.getUsername());
+        }
+        return new ResultResponse<>();
+    }
+
 
 }
