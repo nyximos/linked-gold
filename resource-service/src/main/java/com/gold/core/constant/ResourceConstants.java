b8c0cac8d427ce29b4eb313a5d12105cfea6b8d5
nyximos <nyximos@gmail.com>
2024-09-08 16:31:09 +0900

    feat: ResourceConstants 추가 (#8)

--- resource-service/src/main/java/com/gold/core/constant/ResourceConstants.java
+++ resource-service/src/main/java/com/gold/core/constant/ResourceConstants.java
@@ -1,2 +1,6 @@
-package com.gold.core.constant;public class ResourceConstants {
+package com.gold.core.constant;
+
+public class ResourceConstants {
+    public static final String HEADER_AUTHORIZE_TOKEN = "Authorization";
+    public static final String TOKEN_USER = "TokenUser";
 }
