7dea56f630ede7cec8356b72040edbb77c7b8ceb
nyximos <nyximos@gmail.com>
2024-09-09 20:55:33 +0900

    refactor: 토큰 컨트롤러 url 수정 (#10)

--- resource-service/src/main/java/com/gold/resource/controller/api/TokenController.java
+++ resource-service/src/main/java/com/gold/resource/controller/api/TokenController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/apis/token")
+@RequestMapping("/apis/tokens")
 public class TokenController {
 
     private final TokenService tokenService;
