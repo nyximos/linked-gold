6b0b0a3c0eaea8fb3d190e13f0a54b23d2d55af8
nyximos <nyximos@gmail.com>
2024-09-11 21:47:57 +0900

    docs: Controller Swagger 주석 추가 (#17)

--- resource-service/src/main/java/com/gold/resource/controller/api/TokenController.java
+++ resource-service/src/main/java/com/gold/resource/controller/api/TokenController.java
@@ -4,6 +4,8 @@ import com.gold.core.constant.ResourceConstants;
 import com.gold.core.wrapper.ResultResponse;
 import com.gold.resource.controller.model.response.TokenModel;
 import com.gold.resource.service.TokenService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -13,11 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/apis/token")
+@Tag(name = "토큰 API", description = "JWT 토큰 관련 작업을 처리합니다.")
 public class TokenController {
 
     private final TokenService tokenService;
 
     @PostMapping("/reissue")
+    @Operation(summary = "토큰 재발급", description = "리프레시 토큰으로 새로운 엑세스 토큰을 발급합니다.")
     public ResultResponse<TokenModel> reIssue(@RequestHeader(ResourceConstants.HEADER_AUTHORIZE_TOKEN) String refreshToken) {
         return new ResultResponse<>(tokenService.reIssue(refreshToken));
     }
--- resource-service/src/main/java/com/gold/resource/controller/api/UserController.java
+++ resource-service/src/main/java/com/gold/resource/controller/api/UserController.java
@@ -5,6 +5,8 @@ import com.gold.resource.controller.model.request.LoginRequestModel;
 import com.gold.resource.controller.model.request.SignUpRequestModel;
 import com.gold.resource.controller.model.response.TokenModel;
 import com.gold.resource.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,17 +16,20 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/apis/users")
+@Tag(name = "사용자 API", description = "사용자 회원가입 및 로그인 관련 작업을 처리합니다.")
 public class UserController {
 
     private final UserService userService;
 
     @PostMapping
+    @Operation(summary = "회원가입", description = "회원가입을 합니다..")
     public ResultResponse<Void> signUp(@Valid @RequestBody SignUpRequestModel signUpRequestModel) {
         userService.signUp(signUpRequestModel);
         return new ResultResponse<>();
     }
 
     @PostMapping("/login")
+    @Operation(summary = "로그인", description = "로그인 후 토큰을 발급받습니다.")
     public ResultResponse<TokenModel> signIn(@Valid @RequestBody LoginRequestModel loginRequestModel) {
         return new ResultResponse(userService.signIn(loginRequestModel));
     }
