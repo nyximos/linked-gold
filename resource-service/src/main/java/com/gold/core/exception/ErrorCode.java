51796d5690a09bc3125d84efd42cc744e483844b
nyximos <nyximos@gmail.com>
2024-09-11 14:38:31 +0900

    refactor: 재고 부족 에러 코드 수정 (#16)

--- resource-service/src/main/java/com/gold/core/exception/ErrorCode.java
+++ resource-service/src/main/java/com/gold/core/exception/ErrorCode.java
@@ -14,7 +14,8 @@ public enum ErrorCode {
     INVALID_TOKEN("유효하지 않은 토큰입니다."),
     INVALID_REFRESH_TOKEN("유효하지 않은 리프레시 토큰입니다."),
     GOLD_NOT_FOUND_EXCEPTION("상품을 찾을 수 없습니다."),
-    UNKNOWN_INVOICE_TYPE("주문 타입을 알 수 없습니다.")
+    UNKNOWN_INVOICE_TYPE("주문 타입을 알 수 없습니다."),
+    INSUFFICIENT_WEIGHT_EXCEPTION("재고가 부족합니다."),
     ;
 
     private String defaultMessage;
