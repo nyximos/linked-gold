2c3755a12259d2432eb63fca03c47979b0779a11
nyximos <nyximos@gmail.com>
2024-09-11 22:00:08 +0900

    docs: Request, Response Swagger 주석 추가 (#17)

--- resource-service/src/main/java/com/gold/resource/controller/model/request/LoginRequestModel.java
+++ resource-service/src/main/java/com/gold/resource/controller/model/request/LoginRequestModel.java
@@ -1,5 +1,6 @@
 package com.gold.resource.controller.model.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
@@ -9,10 +10,12 @@ import lombok.Setter;
 @Setter
 public class LoginRequestModel {
 
+    @Schema(description = "이메일 주소", example = "test@google.com")
     @NotNull(message = "email은 null이 될 수 없습니다.")
     @Email(message = "유효한 이메일 주소를 입력해 주세요.")
     private String email;
 
+    @Schema(description = "비밀번호", example = "password123")
     @NotNull(message = "password는 null이 될 수 없습니다.")
     private String password;
 }
--- resource-service/src/main/java/com/gold/resource/controller/model/response/TokenModel.java
+++ resource-service/src/main/java/com/gold/resource/controller/model/response/TokenModel.java
@@ -1,5 +1,6 @@
 package com.gold.resource.controller.model.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 @Getter
@@ -8,6 +9,11 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class TokenModel {
+
+    @Schema(description = "엑세스 토큰")
     private String accessToken;
+
+    @Schema(description = "리프레시 토큰")
     private String refreshToken;
+
 }
