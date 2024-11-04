4a4df60d2285ea6ab6bb287a33e9cb3e5e507718
nyximos <76250754+nyximos@users.noreply.github.com>
2024-11-04 22:40:28 +0900

    Update start.sh

--- script/start.sh
+++ script/start.sh
@@ -1,22 +1,18 @@
-# start.sh
+#!/bin/bash
 
-PROJECT_ROOT="/home/ec2-user/deploy"
-JAR_FILE="$PROJECT_ROOT/server-0.0.1-SNAPSHOT.jar"
+SERVICE_NAME=$1
+PROJECT_ROOT="/home/ec2-user/linked-gold/$SERVICE_NAME"
+JAR_FILE="$PROJECT_ROOT/build/libs/$SERVICE_NAME-0.0.1-SNAPSHOT.jar"
 
 APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
-TIME_NOW=$(date +%c)
+TIME_NOW=$(date '+%Y-%m-%d %H:%M:%S')
 
-# build 파일 복사
-echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
-cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
-
-# jar 파일 실행
-echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
-source ~/.bashrc
+# JAR 파일 실행
+echo "$TIME_NOW > Starting $SERVICE_NAME..." >> $DEPLOY_LOG
 nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
 
-CURRENT_PID=$(pgrep -f $JAR_FILE)
-echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
+CURRENT_PID=$!
+echo "$TIME_NOW > $SERVICE_NAME started with PID $CURRENT_PID" >> $DEPLOY_LOG
