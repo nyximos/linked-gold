5287afdd90d879332c73dace515fa5d2d5d1b7f9
nyximos <76250754+nyximos@users.noreply.github.com>
2024-11-05 23:52:27 +0900

    Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -7,6 +7,11 @@ JAR_FILE="$PROJECT_ROOT/build/libs/$SERVICE_NAME-0.0.1-SNAPSHOT.jar"
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 TIME_NOW=$(date '+%Y-%m-%d %H:%M:%S')
 
+# 로그 파일이 존재하지 않으면 생성
+if [ ! -f "$DEPLOY_LOG" ]; then
+  touch "$DEPLOY_LOG"
+fi
+
 # 현재 실행 중인 애플리케이션 PID 확인 후 종료
 CURRENT_PID=$(pgrep -f $JAR_FILE)
 
